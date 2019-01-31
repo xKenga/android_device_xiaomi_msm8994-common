daba56369ac2a710946a64fed1c422b41e7e95e1
xKenga <yourkenga@yandex.ru>
2019-01-31 12:56:32 +0300

    cleanup
    Change-Id: Iab3bb12f9451f2c87c93d515a5e6112d4c10fe3c

--- msm8994-common.mk
+++ msm8994-common.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm
+    vendor.lineage.livedisplay@1.0-service-legacymm
 
 # OMX
 PRODUCT_PACKAGES += \
