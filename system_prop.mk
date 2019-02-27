497d0a722b209e24214dda938c71b41dd20206bf
xKenga <yourkenga@yandex.ru>
2019-02-27 11:21:22 +0300

    msm8994-common: Disable fluence for speaker

--- system_prop.mk
+++ system_prop.mk
@@ -193,7 +193,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.speaker=false \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7
 
